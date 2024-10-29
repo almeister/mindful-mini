b347fa64f26ab9b6858a396e74c5fd98529c8632
Alex C <ALEX.COATON@GMAIL.COM>
2024-10-29 21:12:35 +1300

    Refactor legend and refine responsive layout

---
+++ DETAILS-IMPROVEMENTS.md
@@ -0,0 +1,5 @@
+# Future Improvements for the Details Page
+
+1. Keep text to a minimum for Summary sections (eg: Fund Details, Ethical Investment, Avoiding Harmful Companies).
+2. Instead of text, use more interactive elements like Ethical Investment badges with clickable tooltips.
+3.
--- kiwisaver/views.py
+++ kiwisaver/views.py
@@ -18,4 +18,15 @@ class DetailView(generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["categories"] = Category.objects.all()
+
+        # TODO: Move legend data to a model
+        chart_legend = [
+        {"color": "yellow", "text": "Human Rights Violations"},
+        {"color": "jade", "text": "Environmental Harm"},
+        {"color": "raspberry", "text": "Animal Cruelty"},
+        {"color": "brown", "text": "Weapons"},
+        {"color": "purple", "text": "Fossil Fuels"},
+        {"color": "violet", "text": "Social Harm"},
+        ]
+        context["chart_legend"] = chart_legend
         return context
